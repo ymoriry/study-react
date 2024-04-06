544c5abb29905d87ce071440b268b4666cfa66c6
大森優 <>
2024-04-06 10:15:09 +0900

    ✨ Headerの追加

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+        <Link href="/">
+            <p className={styles.link}>Index</p>
+        </Link>
+        <Link href="/about">
+            <p className={styles.link}>About</p>
+        </Link>
+    </header>
+  );
+}
