6357aa1ee93e3bae5e14ecc24854ce43b2bb7a94
大森優 <>
2024-04-06 11:16:25 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -4,6 +4,29 @@ import styles from "./Links.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Docs →",
+    description: "Find in-depth information about Next.js features and&nbsp;API."
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Learn →",
+    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!"
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Templates →",
+    description: "Discover and deploy boilerplate example Next.js&nbsp;projects."
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy <span>-&gt;</span>",
+    description: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel."
+  }
+]
+
 export function Links() {
   return (
     <>
@@ -19,62 +42,24 @@ export function Links() {
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.title}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={styles.description}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.title}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={styles.description}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.title}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={styles.description}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.title}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={styles.description}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          {ITEMS.map(item => {
+            return (
+              <a
+                key={item.href}
+                href={item.href}
+                className={styles.card}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2 className={styles.title}>
+                  {item.title}
+                </h2>
+                <p className={styles.description}>
+                  {item.description}
+                </p>
+              </a>
+            )
+          })}
         </div>
     </>
   );
